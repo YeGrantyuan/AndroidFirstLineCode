ca619ab4318f0e2c74692587574772176d9307d6
Grantyuan <614612536@qq.com>
2017-10-31 18:08:21 +0800

    1.修改了DataBaseTest，练习内容提供器；2.添加CameraAlbumTest工程

--- DataBaseTest/app/src/main/java/com/grantyuan/databasetest/MainActivity.java
+++ DataBaseTest/app/src/main/java/com/grantyuan/databasetest/MainActivity.java
@@ -1,72 +1,56 @@
 package com.grantyuan.databasetest;
 
 import android.content.ContentValues;
-import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.grantyuan.databasetest.databaseclass.MyDatabaseHelper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private MyDatabaseHelper dbHelper;
+
+    private String newId;
+
+    private Button addData;
+    private Button queryData;
+    private Button updateData;
+    private Button deleteData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        dbHelper = new MyDatabaseHelper(this, "Book.db", null, 1);
-        dbHelper = new MyDatabaseHelper(this, "Book.db", null, 6);
-        Button createDatabase = (Button) findViewById(R.id.create_database);
-        createDatabase.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dbHelper.getWritableDatabase();
-            }
-        });
 
-        Button addData = (Button) findViewById(R.id.add_data);
-        addData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SQLiteDatabase db = dbHelper.getWritableDatabase();
+        addData = (Button)findViewById(R.id.add_data);
+        queryData = (Button)findViewById(R.id.query_database);
+        updateData = (Button)findViewById(R.id.updata_data);
+        deleteData = (Button)findViewById(R.id.delete_data);
+
+        addData.setOnClickListener(this);
+        queryData.setOnClickListener(this);
+        updateData.setOnClickListener(this);
+        deleteData.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.add_data:
+                Uri uri = Uri.parse("content://com.grantyuan.databasetest.provider/book");
                 ContentValues values = new ContentValues();
-                //开始组装第一条数据
-                values.put("name", "The Da Vinci Code");
-                values.put("author", "Dan Brown");
-                values.put("pages", 454);
-                values.put("prices", 16.96);
-                db.insert("Book", null, values);
-                values.clear();
-                //开始组装第二条数据
-                values.put("name", "The Lost Symbol");
-                values.put("author", "Dan Brown");
-                values.put("pages", 510);
-                values.put("prices", 19.95);
-                db.insert("Book", null, values);
-            }
-        });
-
-        Button updataData = (Button) findViewById(R.id.updata_data);
-        updataData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SQLiteDatabase db = dbHelper.getWritableDatabase();
-                ContentValues values = new ContentValues();
-                values.put("prices", 10.99);
-                db.update("Book", values, "'name'=?", new String[] {"The Da Vinci Code"});
-            }
-        });
-
-        Button deleteData = (Button) findViewById(R.id.delete_data);
-        deleteData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SQLiteDatabase db = dbHelper.getWritableDatabase();
-                db.delete("Book", "'pages' > ?", new String[] {"500"});
-            }
-        });
+                values.put("name", "A Clash of Kings");
+                values.put("author", "George Martin");
+                values.put("pages", 1040);
+                values.put("price", 22.85);
+                Uri newUri = getContentResolver().insert(uri, values);
+                newId = newUri.getPathSegments().get(1);
+                break;
+            default:
+                break;
+        }
     }
 }
